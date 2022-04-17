66aba3f0f1a0ccf299508271acf49c7c7dabc43d
prabhpreet-singh <kamaljitsingh.singh70@gmail.com>
2022-04-17 23:34:23 +0530

    Create twitter.js

---
+++ twitter-clone-api/helpers/twitter.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const dotenv = require('dotenv');
+dotenv.config() ;
+
+const url = "https://api.twitter.com/1.1/search/tweets.json" ;
+
+class Twitter {
+  get(query, resultType, maxId) {
+    return axios.get(url, {
+        params: {
+            q: query,
+            result_type: resultType,
+            count: 10,
+            tweet_mode: "extended",
+            max_id: maxId
+        },
+        headers: {
+            "Authorization": `Bearer ${process.env.TWITTER_API_TOKEN}`
+        }
+    })
+  
+  }
+}
+
+module.exports = Twitter ;
